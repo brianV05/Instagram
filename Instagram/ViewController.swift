256c5202969fa8d0a5e28dad75913d9558ef2df5
Brian Velecela <70337936+brianV05@users.noreply.github.com>
2022-03-08 19:36:56 -0500

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Brian Velecela on 3/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
